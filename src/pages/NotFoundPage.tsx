c7ab71489408125b9f8d4408268ad269e9fec020
Michał Miłek <112391810+michalmilek@users.noreply.github.com>
2023-11-01 01:52:21 +0100

    Update NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-=======
->>>>>>> 8834556 (fix(folderStructure): error-page folder fix)
 import WrongPage from "src/components/error-page-components/wrong-page-error";
 
 const NotFoundPage = () => {
